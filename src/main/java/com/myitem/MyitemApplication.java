a284f05ac2c3af2640ff3632a574384c9d53c0a9
lw <173980943@qq.com>
2020-10-21 15:27:26 +0800

    测试代码

---
+++ src/main/java/com/myitem/MyitemApplication.java
@@ -0,0 +1,14 @@
+package com.myitem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyitemApplication {
+
+    public static void main(String[] args) {
+        //测试代码
+        SpringApplication.run(MyitemApplication.class, args);
+    }
+
+}
